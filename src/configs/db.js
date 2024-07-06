5603831535885c0a019e33d8ff4f142bbfc98396
telkomdev-raga <argadeva@gmail.com>
2024-07-07 05:08:32 +0700

    Config DB Lost Connection Server Handling

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,17 +1,35 @@
 require("dotenv").config();
 
 const mysql = require("mysql");
-const connection = mysql.createConnection({
+const db_config = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME
 });
 
-connection.connect(err => {
-  if (err) {
-    console.log(`Error : ${err}`);
-  }
-});
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config);
+
+  connection.connect(function(err) {
+    if(err) {
+      console.log("error when connecting to db:", err);
+      setTimeout(handleDisconnect, 2000);
+    }
+  });
+
+  connection.on("error", function(err) {
+    console.log("db error", err);
+    if(err.code === "PROTOCOL_CONNECTION_LOST") {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
 
 module.exports = connection;
