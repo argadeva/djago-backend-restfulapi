d2d5a978a647808091e8f43f7d958a7adb3453ea
telkomdev-raga <argadeva@gmail.com>
2024-07-11 20:01:09 +0700

    Change mysql db connection to pool connection

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,32 +2,31 @@ require("dotenv").config();
 
 const mysql = require("mysql");
 const db_config = {
+  connectionLimit: 10,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 };
 
-var connection = mysql.createConnection(db_config);
+var connection = mysql.createPool(db_config);
 
-function handleDisconnect() {
-  console.log("handleDisconnect()");
-  connection.destroy();
-  connection = mysql.createConnection(db_config);
-  connection.connect(function (err) {
-    if (err) {
-      console.log(" Error when connecting to db  (DBERR001):", err);
-      setTimeout(handleDisconnect, 1000);
-    }
-  });
-}
-
-connection.connect(function (err) {
+connection.getConnection((err, con) => {
   if (err) {
-    console.log("Connection is asleep (time to wake it up): ", err);
-    setTimeout(handleDisconnect, 1000);
-    handleDisconnect();
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      console.error("Database connection was closed.")
+    }
+    if (err.code === "ER_CON_COUNT_ERROR") {
+      console.error("Database has too many connections.")
+    }
+    if (err.code === "ECONNREFUSED") {
+      console.error("Database connection was refused.")
+    }
   }
+
+  if (con) con.release();
+
+  return;
 });
 
 module.exports = connection;
